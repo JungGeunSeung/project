4f6723a08ecce844f76b2318d42083c4c7ffd5ca
WIN-VAMVIGQJ6LV\bijou <alfon4869@naver.com>
2024-07-24 16:02:00 +0900

    정근승/다시시작 에 파일 있씁니다

---
+++ 정근승/info.js
@@ -0,0 +1,5 @@
+const id = localStorage.getItem('dataid');
+const pw = localStorage.getItem('datapw');
+
+console.log(id)
+console.log(pw)
--- 정근승/login.js
+++ 정근승/login.js
@@ -1,8 +1,26 @@
-const params = new URLSearchParams(window.location.search); // params 변수에 로그인에서 넘어온 정보를 담는 변수
+window.onload = function () {
+
+    let gsid = 20000302
+    let gspw = 1234
+
+    let submit = document.querySelector('#submit');
+    if (submit) {
+        submit.addEventListener('click', function (event) {
+            event.preventDefault();
+            let id = document.querySelector('#id');
+            let pw = document.querySelector('#pw');
+            if (id.value == gsid && pw.value == gspw) {
+                localStorage.setItem('dataid', id.value);
+                localStorage.setItem('datapw', pw.value);
+                window.location.href = "Main.html";
+            } else {
+                document.querySelector('#falseID').setAttribute('style', 'display: block')
+            }
+        })
+    }
 
 
-
-
+}
 
 
 
