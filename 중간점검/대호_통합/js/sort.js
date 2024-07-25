92e8293e9cf2cd94e55d8f565950e1fe267278ba
bijou <alfon4869>
2024-07-25 22:38:41 +0900

    제품을 엑셀에 추가함, 생산계획목록에 바뀐점 있음, BOM에 가라데이터 추가, sort 자바스크립트 수정, 테이블에 있는 체크박스 선택시 전체 선택 이벤트 추가

--- 중간점검/대호_통합/js/sort.js
+++ 중간점검/대호_통합/js/sort.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const table = document.querySelector('.tableID > table');
     const headers = table.querySelectorAll('th');
-    let currentSortColumn = -1;
-    let isAscending = true;
+    let currentSortColumn = -1; //현재정렬중인 열의 인덱스를 저장
+    let isAscending = true; // 변수 방향 저장 초기값은 true이기때문에 오름차순
 
-    headers.forEach((header, index) => {
+    headers.forEach((header, index) => { //th에 이벤트 적용
         header.addEventListener('click', () => {
-            sortTableByColumn(table, index);
+            if (index === 0) return; // 첫 번째 열(선택 열)은 정렬하지 않음
+            sortTableByColumn(table, index); //함수실행
         });
     });
 
+    // 테이블을 정렬하는 함수
     function sortTableByColumn(table, columnIndex) {
         const tbody = table.querySelector('tbody');
-        const rowsArray = Array.from(tbody.querySelectorAll('tr'));
-        const dataType = headers[columnIndex].dataset.type;
+        const rowsArray = Array.from(tbody.querySelectorAll('tr')); //tr요소를 배열로 변환하여 저장
+        const dataType = headers[columnIndex].dataset.type; //th의 data-type 속성을 저장
 
         // 기본 정렬 방향을 오름차순으로 설정
         if (currentSortColumn === columnIndex) {
