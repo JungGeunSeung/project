072067494e4164a6ddc9d38a20c821249a5875e9
bijou <alfon4869>
2024-07-27 21:05:29 +0900

    오탈자 수정, js 주석

--- project01/JavaScript/sort.js
+++ project01/JavaScript/sort.js
@@ -1,40 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     let table = document.querySelector('.tableID > table');
-    let headers = table.querySelectorAll('th');
-    let currentSortColumn = -1;
-    let isAscending = true;
+    let th = table.querySelectorAll('th');
+    let cnt = -1;
+    let bool = true;
 
-    headers.forEach((header, index) => {
+    th.forEach((header, index) => {
         header.addEventListener('click', () => {
             sortTableByColumn(table, index);
         });
     });
 
-    function sortTableByColumn(table, columnIndex) {
+    function sortTableByColumn(table, idx) {
         let tbody = table.querySelector('tbody');
-        let rowsArray = Array.from(tbody.querySelectorAll('tr'));
-        let dataType = headers[columnIndex].dataset.type;
+        let array = Array.from(tbody.querySelectorAll('tr'));
+        let dataType = th[idx].dataset.type;
 
         // 기본 정렬 방향을 오름차순으로 설정
-        if (currentSortColumn === columnIndex) {
-            isAscending = !isAscending;
+        if (cnt === idx) {
+            bool = !bool;
         } else {
-            isAscending = true;
+            bool = true;
         }
-        currentSortColumn = columnIndex;
+        cnt = idx;
 
-        rowsArray.sort((rowA, rowB) => {
-            let cellA = rowA.children[columnIndex].innerText.trim();
-            let cellB = rowB.children[columnIndex].innerText.trim();
+        array.sort((rowA, rowB) => {
+            let cellA = rowA.children[idx].innerText.trim();
+            let cellB = rowB.children[idx].innerText.trim();
 
             if (dataType === 'number') {
-                return isAscending ? cellA - cellB : cellB - cellA;
+                return bool ? cellA - cellB : cellB - cellA;
             } else {
-                return isAscending ? cellA.localeCompare(cellB) : cellB.localeCompare(cellA);
+                return bool ? cellA.localeCompare(cellB) : cellB.localeCompare(cellA);
             }
         });
 
         // 정렬된 행을 다시 테이블에 추가
-        rowsArray.forEach(row => tbody.appendChild(row));
+        array.forEach(row => tbody.appendChild(row));
     }
 });
