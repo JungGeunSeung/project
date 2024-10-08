b49fa883908ede4928ee40eb858c9bce8a348086
김소원 <sowon0226@gmail.com>
2024-10-08 16:24:15 +0900

    SQL 제품 데이터 생성

--- mes_project_03/src/main/java/kr/or/gaw/controller/SoController.java
+++ mes_project_03/src/main/java/kr/or/gaw/controller/SoController.java
@@ -1,5 +1,15 @@
 package kr.or.gaw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import kr.or.gaw.service.EmpService;
+
+@Controller
 public class SoController {
 
+	@Autowired
+	EmpService empService;
+	
+	
 }
